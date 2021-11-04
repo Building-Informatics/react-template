18b6e2fb903e5bafa4d85901e00b7604cf1552ae
unknown <Administrator@ea01c084.UGent.be>
2021-11-04 17:12:21 +0100

    added css-loader and illustrative button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import {Button} from 'react-bootstrap'
 const App = () => {
     return (
         <div>
             I am the app
+            <Button onClick={()=> {console.log('click')}}>Click me</Button>
         </div>
     )
 }
